96a52bc4198cb8af590dfcd061256683ada32f3d
Illya-Irina <54513447+Illya-Irina@users.noreply.github.com>
2026-02-06 17:33:37 +0100

    Update Qwen_Edit_AIO_NSFW.sh

--- Qwen_Edit_AIO_NSFW.sh
+++ Qwen_Edit_AIO_NSFW.sh
@@ -28,7 +28,7 @@ WORKFLOWS=(
 )
 
 CHECKPOINT_MODELS=(
-    "https://huggingface.co/Phr00t/Qwen-Image-Edit-Rapid-AIO/resolve/main/v19/Qwen-Rapid-AIO-NSFW-v19.safetensors?download=true"
+    "https://huggingface.co/Phr00t/Qwen-Image-Edit-Rapid-AIO/resolve/main/v23/Qwen-Rapid-AIO-NSFW-v23.safetensors?download=true"
 )
 
 UNET_MODELS=(
